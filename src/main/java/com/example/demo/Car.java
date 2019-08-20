83c8974d3030deb02478b4472c223adb89aa7b27
tchekol1 <tchekol1@umbc.edu>
2019-08-20 11:39:54 -0400

    ex2.04

---
+++ src/main/java/com/example/demo/Car.java
@@ -0,0 +1,42 @@
+package com.example.demo;
+
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class Car {
+    @NotNull
+    @Size(min=1, max=20)
+    private String name;
+    @NotNull
+    @Size(min=4, max=15)
+    private String type;
+    @NotNull
+    @Size(min=15,max=40)
+    private String description;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
